e7ec0f15e3be1af6d3bb4b837d57ef95f78e3737
yaroslav <yaroslav.karpenko.dev@gmail.com>
2026-04-20 21:48:27 +0300

    work with gauge

--- CalcOfCarma.Shared/Components/Gauge.razor.cs
+++ CalcOfCarma.Shared/Components/Gauge.razor.cs
@@ -4,6 +4,6 @@ namespace CalcOfCarma.Shared.Components;
 
 public partial class Gauge : ComponentBase
 {
-    [Parameter(CaptureUnmatchedValues = true)]
-    public Dictionary<string, object> AdditionalAttributes { get; set; } = new();
+    [Parameter]
+    public double CurrentAngle { get; set; } = 0;
 }
